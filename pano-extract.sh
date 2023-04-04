032295c6b4adcf2f1197bd364e4a0cfa3a1df9d4
Doclic <doclic@tutanota.com>
2023-04-04 10:18:24 +0000

    Add pbin extraction script

---
+++ pano-extract.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+# Variables
+if [ ! -v INPUT_PBIN ]; then
+    INPUT_PBIN="./code.pbin"
+fi
+
+if [ ! -v OUTPUT_DIR ]; then
+    OUTPUT_DIR="./"
+fi
+
+if [ ! -v TEMP_ZIP ]; then
+    TEMP_ZIP="/tmp/doclic-panorama-extract.zip"
+fi
+
+# File checks
+if [ ! -f $INPUT_PBIN ]; then
+    echo "File $INPUT_PBIN doesn't exist"
+    exit 1
+fi
+
+if [ ! -d $OUTPUT_DIR ]; then
+    echo "Directory $OUTPUT_DIR doesn't exist"
+    exit 1
+fi
+
+if [ -f $TEMP_ZIP ]; then
+    echo "File $TEMP_ZIP already exists"
+    exit 1
+fi
+
+
+
+# Extract
+tail -c +517 $INPUT_PBIN > $TEMP_ZIP
+unzip -d $OUTPUT_DIR $TEMP_ZIP > /dev/null
+rm $TEMP_ZIP
